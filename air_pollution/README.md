f6ac99fcea2a0543acf74954e537ab1d459beea3
LXX <rillxx@163.com>
2016-12-22 03:37:20 -0500

    add new pages

--- air_pollution/README.md
+++ air_pollution/README.md
@@ -1,2 +1,4 @@
 # air_pollution
 for Database 412 final
+
+Front end reference: http://cdn.simplesite.com/
---
+++ air_pollution/public_php/lower_body.php
@@ -0,0 +1,73 @@
+<?php
+
+    <div id="right" class="span3">
+                    <div class="sidebar">
+                        <div class="wrapper viral-box">
+                            <div class="heading wordwrapfix">
+                                <h4>Select city</h4>
+                            </div>
+                            <div class="content">
+                                <p>City dropdown list here</p>
+                                <div class="bottom">
+                                    <a rel="nofollow"  href="" class="btn btn-block">Select</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrapper viral-box">
+                            <div class="heading wordwrapfix">
+                                <h4>Select date range</h4>
+                            </div>
+                            <div class="content">
+                                <p>Date picker here</p>
+                                <div class="bottom">
+                                    <a rel="nofollow"  href="" class="btn btn-block">Select</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrapper viral-box">
+                            <div class="heading wordwrapfix">
+                                <h4>Select pollution level</h4>
+                            </div>
+                            <div class="content">
+                                <p>Select Buttons here</p>
+                                <div class="bottom">
+                                    <a rel="nofollow"  href="" class="btn btn-block">Select</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  <!-- the controller has determined which view to be shown in the content -->
+    <div class="container-fluid footer-wrapper" id="footer"> <!-- this is the Footer Wrapper -->
+        <div class="container">
+            <div class="footer-info" style="margin-bottom: 25px">
+                <div class="footer-info-text">
+                    Database 415 Final Project </br>
+                    By Xiaoxiao Liu & Da Chen
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- this is the Footer content -->
+</div>
+</body>
+<style>
+    .nav li ul{
+        display: none;
+    }
+    .nav li:hover ul{
+        display: block;
+        position: absolute; left: 0px; top: 21px;
+
+    }
+    .nav li{
+        position: relative;
+
+    }
+</style>
+</html>
+
+
+?>
